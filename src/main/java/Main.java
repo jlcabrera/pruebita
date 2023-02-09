aa0e72b45c1e61c3fc0fecffa7598b2167bd9ac0
sansanroj <sansanroj@gmail.com>
2023-02-09 22:03:24 +0100

    modificación febrero

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
 public class Main {
 
-    private static void main(String [] args){
+    public static void main(String [] args){
+
+
         System.out.println("Hello World!");
+
+        int num1 = 5;
+        int num2 = 8;
+
+        int suma = num1 + num2;
+
+        System.out.println(suma);
+
     }
 
 }
